bb44f616c0f857cdb8f948288c49ef8df01c596e
JashwanthReddy <jashwanthreddy927@gmail.com>
2026-03-10 16:45:25 +0530

    new commits

--- SkillForgeLibrary/SkillForgeLibrary/obj/Debug/net10.0/SkillForgeLibrary.AssemblyInfo.cs
+++ SkillForgeLibrary/SkillForgeLibrary/obj/Debug/net10.0/SkillForgeLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkillForgeLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d112461341f5d21b030d6c4b67a9265d2906b675")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296f5043fe651b469102c221f885c71aa9e91d9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkillForgeLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkillForgeLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
