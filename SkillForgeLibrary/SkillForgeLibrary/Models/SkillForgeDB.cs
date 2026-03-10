d112461341f5d21b030d6c4b67a9265d2906b675
JashwanthReddy <jashwanthreddy927@gmail.com>
2026-03-10 14:47:59 +0530

    new folder

---
+++ SkillForgeLibrary/SkillForgeLibrary/Models/SkillForgeDB.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace SkillForgeLibrary.Models;
+
+public class SkillForgeDB:DbContext
+{
+    public SkillForgeDB(){
+        
+    }
+    public SkillForgeDB(DbContextOptions<SkillForgeDB> options): base(options)
+    {
+        
+    }
+    public virtual DbSet<Course> Courses{ get; set; }
+    public virtual DbSet<Module> Modules { get; set; }
+    public virtual DbSet<User> Trainers { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=LTIN718736\\SQLEXPRESS;Initial Catalog=SkillForgeDB;Integrated Security=True;TrustServerCertificate=True");
+    }
+}
