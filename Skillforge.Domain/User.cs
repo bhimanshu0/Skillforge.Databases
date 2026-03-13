d67e9c73eaa90d5b1a1329b6c6b960ef3eb1c857
jashwanthreddypulugu <Pulugu.JashwanthReddy@cognizant.com>
2026-03-13 18:06:06 +0530

    Make User properties non-nullable
    Updated properties in User class to be non-nullable.

--- Skillforge.Domain/User.cs
+++ Skillforge.Domain/User.cs
@@ -9,15 +9,15 @@ public class User
 {
     [Key]
     [Column(TypeName ="CHAR(5)")]
-    public string? UserID{get; set;}
+    public string UserID{get; set;}
  
     [Column(TypeName ="CHAR(20)")]
     [Required]
-    public string? Name{get; set;}
+    public string Name{get; set;}
  
     [Column(TypeName ="CHAR(20)")]
     [Required]
-    public string? Role{get; set;}
+    public string Role{get; set;}
  
     [Column(TypeName ="CHAR(50)")]
     [Required, RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$",
@@ -25,11 +25,11 @@ public class User
    
     [Column(TypeName ="CHAR(10)")]
     [Required, RegularExpression(@"^\d{10}$", ErrorMessage = "Phone must be exactly 10 digits.")]
-    public string? Phone{get; set;}
+    public string Phone{get; set;}
  
     [Column(TypeName ="CHAR(15)")]
     [Required]
-    public string? Status{get; set;}
+    public string Status{get; set;}
     //public virtual ICollection<AuditLog> AuditLogs{get; set;}=new List<AuditLog>();
     public virtual ICollection<Course> Courses{get; set;}= new List<Course>();
 }
