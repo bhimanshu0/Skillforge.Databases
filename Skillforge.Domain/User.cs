b598fbc350a32c1a432cfa649fb5217d46cc810e
VaishnaviPirangi04 <Vaishnavi.Pirangi@cognizant.com>
2026-03-16 17:44:30 +0530

    Add Enrollments collection to User class

--- Skillforge.Domain/User.cs
+++ Skillforge.Domain/User.cs
@@ -43,4 +43,5 @@ public class User
     public virtual ICollection<Course> Courses { get; set; } = new List<Course>();
     public virtual ICollection<Result> Results { get; set; } = new List<Result>();
     public virtual ICollection<Certification> Certifications { get; set; } = new List<Certification>();
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
 }
